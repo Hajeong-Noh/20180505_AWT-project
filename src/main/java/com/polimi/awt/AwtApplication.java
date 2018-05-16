71cb17163dd6e4775db8920a1dab03880ede87cd
12Balu34 <bastian.scherber@gmail.com>
2018-05-17 00:24:35 +0200

    Set-up Maven Project via Spring Boot Initializer

---
+++ src/main/java/com/polimi/awt/AwtApplication.java
@@ -0,0 +1,12 @@
+package com.polimi.awt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwtApplication.class, args);
+    }
+}
