d0a92fc22a02283a4f66deb957916079ff544d9f
12Balu34 <bastian.scherber@gmail.com>
2018-05-18 23:00:13 +0200

    added findByName annotation

--- src/main/java/com/polimi/awt/repository/RoleRepository.java
+++ src/main/java/com/polimi/awt/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.polimi.awt.repository;
 
 import com.polimi.awt.model.Role;
+import com.polimi.awt.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    Role findByName(RoleName roleName);
 }
