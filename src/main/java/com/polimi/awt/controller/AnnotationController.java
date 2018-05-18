8b9d0b326c31f40f3d63887f5013fb6f007f0b69
Hajeong Noh <sciance@naver.com>
2018-05-18 14:22:17 +0200

    Created controller skeletons

--- src/main/java/com/polimi/awt/controller/AnnotationController.java
+++ src/main/java/com/polimi/awt/controller/AnnotationController.java
@@ -1,15 +1,13 @@
 package com.polimi.awt.controller;
 
-import com.polimi.awt.repository.UserRepository;
+import com.polimi.awt.repository.AnnotationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class UserController {
-
+public class AnnotationController {
     @Autowired
-    private UserRepository userRepository;
+    private AnnotationRepository annotationRepository;
 }
--- src/main/java/com/polimi/awt/controller/PeakController.java
+++ src/main/java/com/polimi/awt/controller/PeakController.java
@@ -1,15 +1,13 @@
 package com.polimi.awt.controller;
 
-import com.polimi.awt.repository.UserRepository;
+import com.polimi.awt.repository.PeakRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class UserController {
-
+public class PeakController {
     @Autowired
-    private UserRepository userRepository;
+    private PeakRepository peakRepository;
 }
