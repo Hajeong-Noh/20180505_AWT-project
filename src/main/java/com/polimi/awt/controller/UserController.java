5d884fa2fa0b8503f3c9faf86adb63efe843f4a3
Hajeong Noh <sciance@naver.com>
2018-05-18 14:30:42 +0200

    Added post mapping to UserController

--- src/main/java/com/polimi/awt/controller/UserController.java
+++ src/main/java/com/polimi/awt/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.polimi.awt.controller;
 
+import com.polimi.awt.model.User;
 import com.polimi.awt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @PostMapping("/users")
+    private User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
 }
