a3b4dab4a81450e36a33b334c948c263f2fb5390
12Balu34 <bastian.scherber@gmail.com>
2018-05-22 00:28:48 +0200

    Added wrapper class for updating user information

---
+++ src/main/java/com/polimi/awt/payload/UpdateUserRequest.java
@@ -0,0 +1,35 @@
+package com.polimi.awt.payload;
+
+public class UpdateUserRequest {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public UpdateUserRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
