d3a219cf303e1c7d1f00f79caca0e4c6055c1844
Hajeong Noh <sciance@naver.com>
2018-05-22 17:40:08 +0200

    Added CampaignController logic

--- src/main/java/com/polimi/awt/controller/CampaignController.java
+++ src/main/java/com/polimi/awt/controller/CampaignController.java
@@ -1,13 +1,30 @@
 package com.polimi.awt.controller;
 
+import com.polimi.awt.model.Campaign;
+import com.polimi.awt.model.users.Manager;
 import com.polimi.awt.repository.CampaignRepository;
+import com.polimi.awt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CampaignController {
     @Autowired
     private CampaignRepository campaignRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/campaigns")
+    private List<Campaign> getCampaigns() {
+        return campaignRepository.findAll();
+    }
+
+    @PostMapping("/campaigns")
+    private Campaign createCampaign(@RequestParam (value = "id") Long managerId, @RequestParam (value = "name") String name) {
+        Manager manager = (Manager) userRepository.findUserById(managerId);
+        return manager.createCampaign(name);
+    }
 }
--- src/main/java/com/polimi/awt/model/Campaign.java
+++ src/main/java/com/polimi/awt/model/Campaign.java
@@ -3,6 +3,7 @@ package com.polimi.awt.model;
 import com.polimi.awt.model.users.Manager;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,14 +14,80 @@ public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String name;
 
     @Enumerated(EnumType.STRING)
     private CampaignStatus campaignStatus;
+
+    @NotBlank
     private Date startDate;
+
     private Date endDate;
+
     @OneToMany(mappedBy = "campaign", fetch = FetchType.EAGER)
     private Set <Peak> peakSet = new HashSet<>();
+
     @ManyToOne
     private Manager manager;
+
+    public Campaign() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CampaignStatus getCampaignStatus() {
+        return campaignStatus;
+    }
+
+    public void setCampaignStatus(CampaignStatus campaignStatus) {
+        this.campaignStatus = campaignStatus;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Set<Peak> getPeakSet() {
+        return peakSet;
+    }
+
+    public void setPeakSet(Set<Peak> peakSet) {
+        this.peakSet = peakSet;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
