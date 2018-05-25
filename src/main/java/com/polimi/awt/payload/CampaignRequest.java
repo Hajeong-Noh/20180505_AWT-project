e5b0b3d4f65d46f6f808343b5dcdb6c39cd81980
12Balu34 <bastian.scherber@gmail.com>
2018-05-26 00:59:43 +0200

    Refactored id to userId

--- src/main/java/com/polimi/awt/payload/CampaignRequest.java
+++ src/main/java/com/polimi/awt/payload/CampaignRequest.java
@@ -1,14 +1,14 @@
 package com.polimi.awt.payload;
 
 public class CampaignRequest {
-    private Long id;
+    private Long userId;
 
-    public Long getId() {
-        return id;
+    public Long getUserId() {
+        return userId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     private String name;
