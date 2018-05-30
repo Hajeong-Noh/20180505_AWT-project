69ea7c1eea309eb9ab5c07b0f70be4b1633ae6a1
12Balu34 <bastian.scherber@gmail.com>
2018-05-30 22:34:21 +0200

    Adapted campaign classes to secure endpoints

--- src/main/java/com/polimi/awt/controller/CampaignController.java
+++ src/main/java/com/polimi/awt/controller/CampaignController.java
@@ -2,48 +2,69 @@ package com.polimi.awt.controller;
 
 import com.polimi.awt.model.Campaign;
 import com.polimi.awt.model.users.Manager;
+import com.polimi.awt.model.users.User;
 import com.polimi.awt.model.users.Worker;
 import com.polimi.awt.payload.CampaignRequest;
 import com.polimi.awt.repository.CampaignRepository;
 import com.polimi.awt.repository.UserRepository;
+import com.polimi.awt.security.CurrentUser;
+import com.polimi.awt.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.polimi.awt.model.RoleName.MANAGER;
+
 @RestController
 @RequestMapping("/api")
 public class CampaignController {
+
     @Autowired
     private CampaignRepository campaignRepository;
+
     @Autowired
     private UserRepository userRepository;
 
+
     @GetMapping("/campaigns")
-    private List<Campaign> getCampaigns() {
+    public List<Campaign> getCampaigns(@CurrentUser UserPrincipal currentUser) {
+        User user = userRepository.findUserById(currentUser.getId());
+        if (user.rolesContainsRoleName(MANAGER)) {
+            return campaignRepository.findAllByManager_Id(user.getId());
+        }
+        //TODO: return enrolled Campaigns if user is a worker
         return campaignRepository.findAll();
     }
 
     @GetMapping("/campaigns/{campaignId}")
-    private Campaign getCampaignById (@PathVariable Long campaignId) {
-       return campaignRepository.findCampaignById(campaignId);
+    public Campaign getCampaignById (@PathVariable Long campaignId, @CurrentUser UserPrincipal currentUser) {
+        Campaign campaign = campaignRepository.findCampaignById(campaignId);
+        if (campaign.getManager().getId().equals(currentUser.getId())) {
+
+        }
+        return campaignRepository.findCampaignById(campaignId);
     }
 
     @PostMapping("/campaigns")
-    private Campaign createCampaign(@RequestBody CampaignRequest request) {
-        Manager manager = (Manager) userRepository.findUserById(request.getManagerId());
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public Campaign createCampaign(@RequestBody CampaignRequest request, @CurrentUser UserPrincipal currentUser) {
+        Manager manager = (Manager) userRepository.findUserById(currentUser.getId());
         return campaignRepository.save(manager.createCampaign(request.getName()));
     }
 
     @PatchMapping("/campaigns/{campaignId}")
-    private Campaign updateCampaignStatus (@PathVariable Long campaignId, @RequestBody CampaignRequest request) {
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public Campaign updateCampaignStatus (@PathVariable Long campaignId, @RequestBody CampaignRequest request) {
         Manager manager = (Manager) userRepository.findUserById(request.getManagerId());
         Campaign campaign = campaignRepository.findCampaignById(campaignId);
         return campaignRepository.save(manager.updateCampaignStatus(campaign));
     }
 
     @PostMapping("/campaigns/{campaignId}")
-    private void enrollInCampaign(@PathVariable Long campaignId, @RequestBody CampaignRequest request) {
+    @PreAuthorize("hasAuthority('WORKER')")
+    public void enrollInCampaign(@PathVariable Long campaignId, @RequestBody CampaignRequest request) {
         Worker worker = (Worker) userRepository.findUserById(request.getWorkerId());
         Campaign campaign = campaignRepository.findCampaignById(campaignId);
         campaignRepository.save(worker.enrollInCampaign(campaign));
