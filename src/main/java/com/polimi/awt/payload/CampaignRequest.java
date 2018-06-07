b481bd39b50296fcbb0077aef618437b559a53c0
12Balu34 <bastian.scherber@gmail.com>
2018-06-07 11:58:40 +0200

    Removed fields that are no longer used

--- src/main/java/com/polimi/awt/payload/CampaignRequest.java
+++ src/main/java/com/polimi/awt/payload/CampaignRequest.java
@@ -1,35 +1,7 @@
 package com.polimi.awt.payload;
 
 public class CampaignRequest {
-    private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    private Long managerId;
-
-    public Long getManagerId() {
-        return managerId;
-    }
-
-    public void setManagerId(Long managerId) {
-        this.managerId = managerId;
-    }
-
-    private Long workerId;
-
-    public Long getWorkerId() {
-        return workerId;
-    }
-
-    public void setWorkerId(Long workerId) {
-        this.workerId = workerId;
-    }
 
     private String name;
 
@@ -39,15 +11,5 @@ public class CampaignRequest {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    private Long roleId;
-
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
     }
 }
