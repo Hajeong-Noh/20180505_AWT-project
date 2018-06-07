b1503ea6af2797b7f70e17ec18c98d9c15cae220
12Balu34 <bastian.scherber@gmail.com>
2018-06-07 22:37:01 +0200

    Added constructor, getters and setters

--- src/main/java/com/polimi/awt/model/PeakConflict.java
+++ src/main/java/com/polimi/awt/model/PeakConflict.java
@@ -2,7 +2,47 @@ package com.polimi.awt.model;
 
 public class PeakConflict {
 
-    private Long peakID;
+    private Long peakId;
+    private String peakName;
     private int numberOfAnnotationsWithValidPeak;
     private int numberOfAnnotationsWithInvalidPeak;
+
+    public PeakConflict(Long peakId, String peakName, int numberOfAnnotationsWithValidPeak, int numberOfAnnotationsWithInvalidPeak) {
+        this.peakId = peakId;
+        this.peakName = peakName;
+        this.numberOfAnnotationsWithValidPeak = numberOfAnnotationsWithValidPeak;
+        this.numberOfAnnotationsWithInvalidPeak = numberOfAnnotationsWithInvalidPeak;
+    }
+
+    public Long getPeakId() {
+        return peakId;
+    }
+
+    public void setPeakId(Long peakId) {
+        this.peakId = peakId;
+    }
+
+    public String getPeakName() {
+        return peakName;
+    }
+
+    public void setPeakName(String peakName) {
+        this.peakName = peakName;
+    }
+
+    public int getNumberOfAnnotationsWithValidPeak() {
+        return numberOfAnnotationsWithValidPeak;
+    }
+
+    public void setNumberOfAnnotationsWithValidPeak(int numberOfAnnotationsWithValidPeak) {
+        this.numberOfAnnotationsWithValidPeak = numberOfAnnotationsWithValidPeak;
+    }
+
+    public int getNumberOfAnnotationsWithInvalidPeak() {
+        return numberOfAnnotationsWithInvalidPeak;
+    }
+
+    public void setNumberOfAnnotationsWithInvalidPeak(int numberOfAnnotationsWithInvalidPeak) {
+        this.numberOfAnnotationsWithInvalidPeak = numberOfAnnotationsWithInvalidPeak;
+    }
 }
