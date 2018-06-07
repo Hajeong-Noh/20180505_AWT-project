69efc500da6d7f2130cefdc6a9bdc0de4c8fb46e
12Balu34 <bastian.scherber@gmail.com>
2018-06-07 22:36:29 +0200

    Added methods needed for Campaign statistics

--- src/main/java/com/polimi/awt/repository/PeakRepository.java
+++ src/main/java/com/polimi/awt/repository/PeakRepository.java
@@ -2,8 +2,11 @@ package com.polimi.awt.repository;
 
 import com.polimi.awt.model.Peak;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Set;
 
 @Repository
@@ -12,5 +15,22 @@ public interface PeakRepository extends JpaRepository<Peak, Long> {
 
     Peak findPeakById (Long peakId);
 
-    int countPeaksByCampaignId(Long campaignId);
+    int countPeaksByCampaignIdAndToBeAnnotated(Long campaignId, boolean toBeAnnotated);
+
+    @Query (value = "select distinct(p.id), p.elevation, p.longitude, p.latitude, p.name, p.provenance, p.to_be_annotated, p.campaign_id   from " +
+            "peak p join annotation a on p.id = a.peak_id where p.campaign_id = :campaignId",
+            nativeQuery = true)
+    List<Peak> findAnnotatedPeaksByCampaignId(@Param(value = "campaignId") Long campaignId);
+
+    @Query (value = "select distinct(p.id), p.elevation, p.longitude, p.latitude, p.name, p.provenance, p.to_be_annotated, p.campaign_id   from " +
+            "peak p join annotation a on p.id = a.peak_id where p.campaign_id = :campaignId and a.is_accepted_by_manager = 0",
+            nativeQuery = true)
+    List<Peak> findAnnotatedPeaksWithRejectedAnnotationByCampaignId(@Param(value = "campaignId") Long campaignId);
+
+    @Query(value = "select count(distinct(peak_id)) count from " +
+            "peak p join annotation a on p.id = a.peak_id " +
+            "where p.campaign_id = :campaignId and a.is_accepted_by_manager = 0",
+            nativeQuery = true)
+    int countAnnotatedPeaksWithRejectedAnnotationByCampaignId(@Param(value = "campaignId") Long campaignId);
+
 }
